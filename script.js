db71e32a0c6badcd79e0d12519073f4c700826f5
bszeidl <szeidl.balazs@hotmail.com>
2022-12-17 14:31:34 +0100

    The triangle consisting of rectangles are ready.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+
+    let size = 50;
+    let positionX = 15;
+    let positionY = 15;
+    let gap = 5;
+
+    for (i = 0; i < 5; i++) { 
+              
+        for (k = 0; k <= i; k++) {
+            context.fillStyle = 'rgba(255,165,0,.5)';
+            context.fillRect(positionX + k*(size+gap), positionY + i*(size+gap),size,size);  
+        }
+        
+    }
 
 }
+
+/*A külső ciklus adja hozzá a sorokat, a belső az oszlopokat. */
 
 window.addEventListener("load", pageLoaded);
